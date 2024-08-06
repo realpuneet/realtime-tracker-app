184b2b659cfd9a82d81b7b21d58557405f5f096a
Puneet Yadav <puneety259@gmail.com>
2024-08-06 14:44:04 +0530

    ejs  change

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const express = require("express");
 const app = express();
 const http = require("http");
 const path = require("path");
-const socketio = require("socket.io");
+const socket_io = require("socket.io");
 const server = http.createServer(app);
-const io = socketio(server);
+const io = socket_io(server);
 
 
 app.set("view engine", "ejs");
@@ -14,7 +14,7 @@ io.on("connection", function(socket){
     socket.on("send-location",function(data){
         io.emit("receive-location",{id: socket.id, ...data});
     });
-    socket.on("discoonect", function(){
+    socket.on("disconnect", function(){
         io.emit("user-disconnected", socket.id)
     });
     console.log("connection established...");
